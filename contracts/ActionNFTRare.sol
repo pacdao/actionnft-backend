f8c12466ffb353c3e94cf73f003e94e08e08cf08
Gerrit Hall <gerrit@rezscore.com>
2021-09-14 12:10:15 -0700

    fix: Split the contracts into two and work on withdraw functions

---
+++ contracts/ActionNFTRare.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ActionNFTRare is ERC721 {
+
+/* Variables */
+    address payable private beneficiary;
+
+/* Rare NFT */
+    uint256 public bidPrice;
+    uint256 public lastBidTime;
+
+    mapping (address => uint256) public bids;
+struct TopBid {
+    uint balance;
+    address addr;
+}
+TopBid[5] public topBidders;
+
+
+
+    bool public auctionEnded = false;
+    uint256 public auctionEndTime;
+   
+/* Accounting Data */ 
+    uint256 public treasuryBalance;
+
+    constructor (
+	    address payable _beneficiary, 
+	    uint256 _bidPrice
+
+    ) public ERC721 ("PACDAO ACTION NFT RARE", "PAC-A1-RARE"){
+	 
+	beneficiary = _beneficiary;
+	bidPrice = _bidPrice;
+
+	_setBaseURI("ipfs://QmcnEZQiGVzPonWS2MENbdY8DkwhWcCW7YBQNk5yHYF112");
+    }
+
+
+    
+/* Bid */
+
+   
+    function bidRare() public payable
+    {
+	  require(auctionEnded == false);
+	  bids[msg.sender] += msg.value;
+	  lastBidTime = now;
+	  updateHighestBidder(msg.sender, bids[msg.sender]);
+
+	  //store_withdrawable(msg.sender, msg.value);
+    }
+
+    function updateHighestBidder(address addr, uint currentValue) internal {
+	    uint i = 0;
+    /** get the index of the current max element **/
+    for(i; i < topBidders.length; i++) {
+        if(topBidders[i].balance < currentValue) {
+            break;
+        }
+    }
+    /** shift the array of position (getting rid of the last element) **/
+    for(uint j = topBidders.length - 1; j > i; j--) {
+        topBidders[j].balance = topBidders[j - 1].balance;
+        topBidders[j].addr = topBidders[j - 1].addr;
+    }
+    /** update the new max element **/
+    topBidders[i].balance = currentValue;
+    topBidders[i].addr = addr;
+}
+
+function isTopBidder(address winner) public view returns (bool) {
+	for(uint _i = 0; _i < 5; _i++) {
+		if(topBidders[_i].addr == winner) {
+			return true;
+		}
+	}
+	return false;
+}
+    
+/* Withdraw Functions */
+	function withdraw() public {
+		require(auctionEnded == true);
+		require(isTopBidder(msg.sender) == false);
+		require(now < auctionEndTime + (60 * 60 * 24 * 30)); 
+		
+	//	uint256 _val = withdrawableBalance[msg.sender];	
+		
+		msg.sender.transfer(bids[msg.sender]);
+//		withdrawableBalance[msg.sender] = 0;
+	}
+	function withdraw_dao() public 
+	{
+		beneficiary.transfer(treasuryBalance);
+		treasuryBalance = 0;
+	}
+
+/* Admin Functions */
+	function updateBeneficiary(address payable _newBeneficiary) public 
+	{		
+		require(msg.sender == beneficiary);
+		beneficiary = _newBeneficiary;
+	}
+	function setTokenUri(uint256 _tokenId, string memory _newUri) public 
+	{
+		require(msg.sender == beneficiary);
+		_setTokenURI(_tokenId, _newUri);
+	}
+	function setDefaultMetadata(string memory _newUri) public 
+	{
+		require(msg.sender == beneficiary);
+		//defaultMetadata = _newUri;
+		_setBaseURI(_newUri);
+	}
+	function setAuctionEnded() public
+	{
+		require(msg.sender == beneficiary);
+		auctionEnded = true;
+		auctionEndTime = now;
+		for(uint _i = 0; _i < topBidders.length; _i++ ) {
+			_safeMint(topBidders[_i].addr, _i);
+		}	
+	}
+
+
+/* Fallback Functions */
+	receive() external payable { }
+	fallback() external payable { }
+
+}
