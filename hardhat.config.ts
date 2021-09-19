ee080a474c37cfa16e5e54fa059c47ebba00ec2d
Chan-Ho Suh <chanhosuh@gmail.com>
2021-09-19 17:24:39 -0400

    Update hardhat config
    - add networks; code is based on env vars which are either exported
      at the command line or from a local file.  Not meant to be opinionated
      here -- change to whatever works best.
    - added setting for the etherscan-verify plugin.  Requires an API key.
    - lastly, had to skip the pre-commit checks since they rely upon
      a Next JS dep that only works in linux

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,24 @@ import 'hardhat-gas-reporter'
 import 'tsconfig-paths/register'
 import 'solidity-coverage'
 import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
 import '@openzeppelin/hardhat-upgrades'
 import { task } from 'hardhat/config'
 import { config as dotenvConfig } from 'dotenv'
 import { resolve } from 'path'
+
+dotenvConfig({ path: resolve(__dirname, "./.env.local") });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("MNEMONIC env var needs to be set.");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("INFURA_API_KEY needs to be set.");
+}
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,6 +31,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   }
 })
 
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -25,4 +40,27 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: '0.8.7',
+  networks: {
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/" + infuraApiKey,
+      accounts: {
+        mnemonic,
+      },
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/" + infuraApiKey,
+      accounts: {
+        mnemonic,
+      },
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/" + infuraApiKey,
+      accounts: {
+        mnemonic,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 }
